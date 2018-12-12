7b9a376ccd3faf12cc11401801dc1a8f60c0d041
BigKwan <jr845911@gmail.com>
2018-12-13 00:34:53 +0800

    update

--- justapp/models.py
+++ justapp/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 from subprocess import check_output
+import subprocess
+from threading import Timer
 # Create your models here.
 
 def portScanner(ip):
     out = check_output(["perl",'/opt/nikto/nikto.pl',"-host",ip])
 
     return out
+
+
